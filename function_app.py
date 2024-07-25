f57ea45037d068a043390254201365f5a339ebec
shreyabatra4 <shreyabatra14@gmail.com>
2024-07-25 16:08:30 -0500

    files

---
+++ function_app.py
@@ -0,0 +1,18 @@
+import azure.functions as func
+import logging
+import time
+
+app = func.FunctionApp()
+
+@app.function_name(name="http_get")
+@app.route(route="http_get")
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    user = req.params.get("user")
+    return f"Hello, {user}!"
+
+@app.function_name(name="http_post")
+@app.route(route="http_post")
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    req_body = req.get_json()
+    name = req_body.get('name')
+    return func.HttpResponse(f"Hello, {name}!")
