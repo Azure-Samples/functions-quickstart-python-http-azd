9e8b43098418b6888ba1a3038e75af378f5c6dd3
Shreya <shbatr@microsoft.com>
2024-08-07 09:22:14 -0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,13 +4,11 @@ import time
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.function_name(name="http_get")
 @app.route(route="http_get")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     user = req.params.get("user")
     return f"Hello, {user}!"
 
-@app.function_name(name="http_post")
 @app.route(route="http_post")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     req_body = req.get_json()
